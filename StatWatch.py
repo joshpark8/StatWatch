05ac943f3028877fbcbbe0d34f45bb9724a571ba
joshpark8 <84344359+joshpark8@users.noreply.github.com>
2021-11-24 01:51:09 -0500

    update StatWatch.py to not show filepath

--- StatWatch.py
+++ StatWatch.py
@@ -1,22 +1,24 @@
 import requests
 import json
 import time
+import pathlib
 
-usernames = ["Park-12632", "GolexFan69-2977", "YZNSA3-2687", "EchoFlex-1763"]
+path = pathlib.Path(__file__).parent.resolve()
+usernames = ["Park-12632", "YZNSA3-2687", "EchoFlex-1763"]
 
 def writeSR(role, sr):
-    f = open(f"C:/Users/wljos/Documents/Coding/projects/StatWatch/logs/{username}_{role}.log", 'a')
+    f = open(f"{path}/logs/{username}_{role}.log", 'a')
     f.write(str(sr) + "\n")
     print(f"updated {username}_{role}.log")
 
 def checkFile(role):
     try:
-        f = open(f"C:/Users/wljos/Documents/Coding/projects/StatWatch/logs/{username}_{role}.log", 'r')
+        f = open(f"{path}/logs/{username}_{role}.log", 'r')
         last_line = f.readlines()[-1:]
         split = str(last_line).split("'")[1].split("\\")[0]
         return int(split)
     except IOError:
-        f = open(f"C:/Users/wljos/Documents/Coding/projects/StatWatch/logs/{username}_{role}.log", 'a')    
+        f = open(f"{path}/logs/{username}_{role}.log", 'a')    
         return -1
 
 def getSR(username, doPrint):
@@ -29,30 +31,34 @@ def getSR(username, doPrint):
     # json_formatted = json.dumps(r, indent=2) #
     # f = open(f"{username}_complete.log", "a")
     # f.write(json_formatted)
+    # print(json_formatted)
+    
+    try:
+        for arr in r['ratings']:
+            role = arr['role']
+            sr = arr['level']
+            fileExists = checkFile(role)
 
-    for arr in r['ratings']:
-        role = arr['role']
-        sr = arr['level']
-        fileExists = checkFile(role)
-
-        if role == 'tank':
-            if doPrint == True:
-                print("\t" + role + ": " + str(sr))
-            if fileExists != sr or fileExists == -1:
-                writeSR(role, sr)
-                tankRating = sr
-        elif role == 'damage':
-            if doPrint == True:
-                print("\t" + role + ": " + str(sr))
-            if fileExists != sr or fileExists == -1:
-                writeSR(role, sr)
-                damageRating = sr
-        elif role == 'support':
-            if doPrint == True:
-                print("\t" + role + ": " + str(sr))
-            if fileExists != sr or fileExists == -1:
-                writeSR(role, sr)
-                supportRating = sr
+            if role == 'tank':
+                if doPrint == True:
+                    print("\t" + role + ": " + str(sr))
+                if fileExists != sr or fileExists == -1:
+                    writeSR(role, sr)
+                    tankRating = sr
+            elif role == 'damage':
+                if doPrint == True:
+                    print("\t" + role + ": " + str(sr))
+                if fileExists != sr or fileExists == -1:
+                    writeSR(role, sr)
+                    damageRating = sr
+            elif role == 'support':
+                if doPrint == True:
+                    print("\t" + role + ": " + str(sr))
+                if fileExists != sr or fileExists == -1:
+                    writeSR(role, sr)
+                    supportRating = sr
+    except TypeError:
+        print("Player has not placed any roles!")
 
 if __name__ == "__main__":
     for username in usernames:
